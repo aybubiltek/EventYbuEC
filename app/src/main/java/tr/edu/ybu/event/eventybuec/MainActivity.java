7c4b63607e73fc49d42791f54d5ae8e25a531d2d
Halid <halidsenyigit@gmail.com>
2018-01-14 00:22:52 +0300

    Http Get Async Main Activity

--- app/src/main/java/tr/edu/ybu/event/eventybuec/MainActivity.java
+++ app/src/main/java/tr/edu/ybu/event/eventybuec/MainActivity.java
@@ -1,13 +1,92 @@
 package tr.edu.ybu.event.eventybuec;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends Activity {
+
+    TextView email, pass;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login login = new Login();
+                login.execute("http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo");
+            }
+        });
+
+
+    }
+
+    private class Login extends AsyncTask<String, Void, String> {
+        int resCode;
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... str) {
+            InputStream in = null;
+            URL url = null;
+            String text = null;
+            try {
+                url = new URL(str[0]);
+                URLConnection urlConn = url.openConnection();
+                HttpURLConnection httpConn = (HttpURLConnection)urlConn;
+                httpConn.setAllowUserInteraction(false);
+                httpConn.setInstanceFollowRedirects(true);
+                httpConn.setRequestMethod("GET");
+                httpConn.connect();
+                resCode = httpConn.getResponseCode();
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    in = httpConn.getInputStream();
+                }
+                InputStreamReader isr = new InputStreamReader(in);
+                int charRead;
+                char[] inputBuffer = new char[2000];
+
+                while ((charRead = isr.read(inputBuffer))>0) {
+                    String readString =
+                            String.copyValueOf(inputBuffer, 0, charRead);
+                    text += readString;
+                    inputBuffer = new char[2000];
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
     }
 }
